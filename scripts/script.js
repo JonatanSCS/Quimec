cf1c32b03cf3c3859dbef259b991c324fad9ab08
JonatanSCS <jonatanscs97@hotmail.com>
2015-10-18 08:26:59 -0500

    Agregando elementos de Quimec para primera revisión con Toño

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
 	$('.ir-arriba').click(function(){
 		$('body, html').animate({
-			scrollTop: '0px'
-		}, 300);
+			scrollTop: '200px'
+		}, 500);
 	});
  
 	$(window).scroll(function(){
-		if( $(this).scrollTop() > 0 ){
+		if( $(this).scrollTop() > 200 ){
 			$('.ir-arriba').slideDown(300);
 		} else {
 			$('.ir-arriba').slideUp(300);
 		}
 	});
- 	
+
+	$(".image-slide")
+        .mouseover(function() { 
+            var src = $(this).attr("src").match(/[^\.]+/) + "over.gif";
+            $(this).attr("src", src);
+        });
+
+	$('.bxslider').bxSlider({
+  		auto: true
+  		
+	});
 });
+
+
