d21490f7d0c6449d7e6db62de1d3556d526854e3
JonatanSCS <jonatanscs97@hotmail.com>
2015-10-19 17:34:37 -0500

    Agregando movimiento a html, minificar archivos

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+
+
 	$('.ir-arriba').click(function(){
 		$('body, html').animate({
 			scrollTop: '200px'
@@ -8,8 +11,10 @@ $(document).ready(function(){
 	$(window).scroll(function(){
 		if( $(this).scrollTop() > 200 ){
 			$('.ir-arriba').slideDown(300);
+
 		} else {
 			$('.ir-arriba').slideUp(300);
+
 		}
 	});
 
@@ -23,6 +28,56 @@ $(document).ready(function(){
   		auto: true
   		
 	});
-});
 
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 200 ){
+			$('.ir-arriba').slideDown(300);
+
+		} else {
+			$('.ir-arriba').slideUp(300);
+
+		}
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 400 ){
+			$(".servicios-item").css("visibility", "visible");
+			$(".servicios-item").css("transition", "opacity 2s");
+			$(".servicios-item").css("opacity", "1");
+			
+
+		} 
+	});
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 1000 ){
+			$(".projectos-imagenes-element").css("visibility", "visible");
+			$(".projectos-imagenes-element").css("transition", "opacity 2s");
+			$(".projectos-imagenes-element").css("opacity", "1");
+			
+
+		} 
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 1500 ){
+			$(".trabajamos-info").css("visibility", "visible");
+			$(".trabajamos-info").css("transition", "margin-left 2s");
+			$(".trabajamos-info").css("margin-left", "20%");
+			$(".trabajamos-info").css("transition", "opacity 2s");
+			$(".trabajamos-info").css("opacity", "1");
+			
+		} 
+	});
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 1900 ){
+			$(".proveedores-info").css("visibility", "visible");
+			$(".proveedores-info").css("transition", "opacity 2s");
+			$(".proveedores-info").css("opacity", "1");
+			
+		} 
+	});	
+
+
+});
 
