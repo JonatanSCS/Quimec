3f00bd83a893bcd7ca51a7f3ffb23d04492280c5
JonatanSCS <jonatanscs97@hotmail.com>
2015-11-01 15:34:33 -0600

    Agregando Grunt JS y sección quienes somos

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+   
+    jade: {
+      debug: {
+        options: {
+          data: {
+          debug: true
+          }
+        },
+        files: {
+          'index.html': 'index.jade',
+          'html/quienes-somos.html':'html/quienes-somos.jade'
+        }
+
+      } 
+    },
+
+    stylus:{
+      debug: {
+        options: {
+          data: {
+          debug: true
+          }
+        },
+        files: {
+          'css/style.css': 'css/style.styl'
+        }
+      } 
+    },
+
+    connect: {
+      server: {
+        options: {
+          hostname: 'localhost',
+          port: 8000,
+          keepalive: true
+        }
+      }
+    },
+    watch: {
+            stylesheets: {
+                files: ['css/style.styl',
+                        'index.jade',
+                        'html/quienes-somos.jade'],
+
+                tasks: ['stylus','jade'],
+                options: {
+                    interrupt: true
+                }
+            }
+        },
+  });
+
+
+
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+  grunt.registerTask('default', ['jade', 'stylus', 'watch']);
+  grunt.registerTask('server', ['connect:server']);
+
+
+
+};
